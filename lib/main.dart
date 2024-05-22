3a9fa214fc88281ff20aabeaf152cff4d7a155a8
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2024-05-23 03:58:21 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:neobis_flutter_figma/features/pages/my_home_page.dart';
+import 'package:neobis_flutter_figma/features/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: HomePage(),
     );
   }
 }
